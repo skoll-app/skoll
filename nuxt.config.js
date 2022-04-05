3d8086693ce2e12474231a4bfd1fe0938c07efab
Adan Florez <adanfb@irisbank.co>
2022-04-05 13:03:54 -0500

    feat: vee-validate was installed

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/vee-validate.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -69,5 +69,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: ['vee-validate'],
+  },
 }
---
+++ plugins/vee-validate.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import { required, email } from 'vee-validate/dist/rules'
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
+
+extend('required', {
+  ...required,
+})

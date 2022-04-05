57932a87f12db901181dbb5eea1af367d93c85e4
Adan Florez <adanfb@irisbank.co>
2022-04-05 08:27:12 -0500

    fix: url import css

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    { src: '/assets/css/bootstrap.min.css' },
-    { src: '/assets/css/style.css' },
+    { src: '~/static/assets/css/bootstrap.min.css' },
+    { src: '~/static/assets/css/style.css' },
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins

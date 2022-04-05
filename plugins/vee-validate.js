39a4c9f5808ce50fa6ee10c16443beaaa9f7e8c6
Adan Florez <adanfb@irisbank.co>
2022-04-05 13:09:23 -0500

    feat: dynamic vali or invalid css clasess

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 
-import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
-import { required, email } from 'vee-validate/dist/rules'
+import { ValidationProvider, ValidationObserver, extend, configure } from 'vee-validate'
+import { required } from 'vee-validate/dist/rules'
 
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
 
 extend('required', {
   ...required,
+})
+
+configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid',
+    // dirty: ['is-dirty', 'is-dirty']
+  }
 })

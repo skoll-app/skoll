c984a4730f952fb22fd044c3cd225fcebbdae494
Adan Florez <adanfb@irisbank.co>
2022-04-18 23:11:12 -0500

    feat: default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Loading />
+    <Toast />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Loading from '~/components/ux/loading/Loading.vue'
+import Toast from '~/components/ux/toast/Toast.vue'
+
+export default Vue.extend({
+  components: { Loading, Toast },
+})
+</script>
+
+<style scoped></style>

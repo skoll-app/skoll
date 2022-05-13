5aae22b59213ea06f14701e3d8320b6e65377723
Adan Florez <adanfb@irisbank.co>
2022-05-13 10:56:49 -0500

    feat: story card

---
+++ pages/feed/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="no-centered">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <ListStoryCard />
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ListStoryCard from '~/components/card/story/List.vue'
+
+export default Vue.extend({
+  layout: 'blue',
+  components: { ListStoryCard },
+})
+</script>
+
+<style scoped lang="scss"></style>

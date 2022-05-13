ed615253769e21846b5b128415efec5066a56535
Adan Florez <adanfb@irisbank.co>
2022-05-13 09:53:42 -0500

    style: blue layuot

---
+++ layouts/blue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header />
+    <div class="pages-content">
+      <Loading />
+      <Toast />
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/header/Header.vue'
+import Loading from '~/components/ux/loading/Loading.vue'
+import Toast from '~/components/ux/toast/Toast.vue'
+
+export default Vue.extend({
+  components: { Loading, Toast, Header },
+  created() {
+    if (process.client) {
+      document.body.style.backgroundColor = '#F0F2F5'
+    }
+  },
+  beforeDestroy () {
+    if (process.client) {
+      document.body.style.backgroundColor = 'var(--bs-white)'
+    }
+  },
+})
+</script>
+
+<style scoped></style>

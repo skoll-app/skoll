9d7a54e7a2a08d8efe445ca5111a09fdc308980f
Adan Florez <adanfb@irisbank.co>
2022-05-21 23:19:37 -0500

    feat: interfaces

---
+++ interfaces/comments.ts
@@ -0,0 +1,8 @@
+import User from "./user"
+
+export default interface Comments {
+  id: string
+  user: User
+  text: string
+  likes: number
+}
---
+++ interfaces/post.ts
@@ -0,0 +1,12 @@
+import Comments from './comments'
+import User from './user'
+
+export default interface Post {
+  id: string
+  user: User
+  location: string
+  imageUrl: string
+  text: string
+  likes: number
+  comments?: Comments[]
+}

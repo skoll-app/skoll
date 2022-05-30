4ea6378f3a51cb5677b5fb8c433dd7d193f370dd
Adan Florez <adanfb@irisbank.co>
2022-05-30 11:53:31 -0500

    refactor: user state improvement

--- store/user/mutations.ts
+++ store/user/mutations.ts
@@ -5,25 +5,7 @@ import User from '../../interfaces/user'
 
 const mutations: MutationTree<UserState> = {
   [UserTypes.SET_USER]: (state, user: User) => {
-    state.firstName = user.firstName
-    state.lastName = user.lastName
-    state.gender = user.gender
-    state.interesGender = user.interesGender
-    state.email = user.email
-    state.cellPhone = user.cellPhone
-    state.cityName = user.cityName
-    state.totalArchievements = user.totalArchievements
-    state.totalSusbscriberMerchant = user.totalSusbscriberMerchant
-    state.amountAvailable = user.amountAvailable
-    state.description = user.description
-    state.logo = user.logo
-    state.age = user.age
-    state.isVerify = user.isVerify
-    state.archivementsAvailable = user.archivementsAvailable
-    state.defaultCard = user.defaultCard
-    state.identification = user.identification
-    state.address = user.address
-    state.cards = user.cards
+    state = Object.assign({}, state, { ...user })
   },
 }
 

1b86a3d9d9a711c03ed59c1c2203383c58925617
adanflorez <adanflorez@users.noreply.github.com>
2022-06-20 12:59:44 +0000

    deploy: 166f3a818d04d552edc4f3c760291440658b76f1

---
+++ _nuxt/static/1655729980/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Faccount\u002Fchange-password","\u002F","\u002Fauth\u002Frecover","\u002Ffeed","\u002Faccount\u002Fhelp","\u002Faccount\u002Fnetwork-linking","\u002Fauth\u002Fregister","\u002Fauth","\u002Faccount\u002Fedit"]})

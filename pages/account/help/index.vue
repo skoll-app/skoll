760aaecac7402e67295468dd03a42578c79ccd8a
Adan Florez <adanfb@irisbank.co>
2022-06-30 20:39:21 -0500

    feat: help view

--- pages/account/help/index.vue
+++ pages/account/help/index.vue
@@ -1,12 +1,46 @@
 <template>
-  <div>Help</div>
+  <div class="card">
+    <div class="card-body">
+      <h4 class="mb-3" v-html="$t('configview.help.title')"></h4>
+      <ValidationObserver tag="form" v-slot="{ invalid }">
+        <TextInput
+          class="mb-3"
+          :placeholder="$t('form.subject')"
+          :name="$t('form.subject')"
+          addVeeClasses
+          rules="required"
+          v-model="subject"
+        />
+        <TextAreaInput
+          class="mb-3"
+          rules="required"
+          addVeeClasses
+          :name="$t('form.message')"
+          :placeholder="$t('form.message')"
+          v-model="message"
+        />
+        <div class="d-flex justify-content-end">
+          <button type="button" class="btn btn-success" :disabled="invalid">
+            {{ $t('form.save') }}
+          </button>
+        </div>
+      </ValidationObserver>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TextAreaInput from '~/components/ux/input/TextAreaInput.vue'
+import TextInput from '~/components/ux/input/TextInput.vue'
 
 export default Vue.extend({
-  layout: 'config'
+  components: { TextInput, TextAreaInput },
+  layout: 'config',
+  data: () => ({
+    subject: '',
+    message: '',
+  }),
 })
 </script>
 

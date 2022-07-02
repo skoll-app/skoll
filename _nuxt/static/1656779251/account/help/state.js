8bb5f38dea3b7629777e31140a433525c397f2fd
adanflorez <adanflorez@users.noreply.github.com>
2022-07-02 17:44:18 +0000

    deploy: 3b5378a3a138253ce69187c7b14e6bfd49851ce5

--- _nuxt/static/1656779251/account/help/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fskoll\u002F_nuxt\u002Fstatic\u002F1656779251",layout:"config",error:a,state:a,serverRendered:false,routePath:"\u002Faccount\u002Fhelp",config:{_app:{basePath:"\u002Fskoll\u002F",assetsPath:"\u002Fskoll\u002F_nuxt\u002F",cdnURL:a}}}}(null));

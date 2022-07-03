38741d9291fd7d0889789ace3baa25dddf8bac85
adanflorez <adanflorez@users.noreply.github.com>
2022-07-03 00:22:33 +0000

    deploy: 8966a2e08b1f8361312cfd14be3ce0a2c93482ff

--- _nuxt/static/1656795808/feed/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fskoll\u002F_nuxt\u002Fstatic\u002F1656795808",layout:"blue",error:a,state:a,serverRendered:false,routePath:"\u002Ffeed",config:{_app:{basePath:"\u002Fskoll\u002F",assetsPath:"\u002Fskoll\u002F_nuxt\u002F",cdnURL:a}}}}(null));

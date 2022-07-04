0c8db21e3aa6b08496df84106fe683ab174e25d0
adanflorez <adanflorez@users.noreply.github.com>
2022-07-04 22:06:20 +0000

    deploy: ce5492da4a40f918d93c0a5b219ece3432576061

---
+++ _nuxt/static/1656972377/account/privacy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fskoll\u002F_nuxt\u002Fstatic\u002F1656972377",layout:"config",error:a,state:a,serverRendered:false,routePath:"\u002Faccount\u002Fprivacy",config:{_app:{basePath:"\u002Fskoll\u002F",assetsPath:"\u002Fskoll\u002F_nuxt\u002F",cdnURL:a}}}}(null));
